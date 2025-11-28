04308fbe54fccfb879e8a6c0e04ed66e6ce79a5c
musfikurzony <musfikurzony@gmail.com>
2025-11-28 15:50:57 +0600

    Create suras.js

---
+++ suras.js
@@ -0,0 +1,46 @@
+// suras.js
+
+window.suras = [
+  {
+    number: 100,
+    nameAr: "سورة العاديات",
+    nameBn: "সূরা আল-আদিয়াত",
+    type: "Makki",
+    ayahCount: 11,
+    file: "surah_100.json"
+  },
+  {
+    number: 101,
+    nameAr: "سورة القارعة",
+    nameBn: "সূরা আল-ক্বারিআহ",
+    type: "Makki",
+    ayahCount: 11,
+    file: "surah_101.json"
+  },
+  {
+    number: 102,
+    nameAr: "سورة التكاثر",
+    nameBn: "সূরা আত-তাকাসুর",
+    type: "Makki",
+    ayahCount: 8,
+    file: "surah_102.json"
+  },
+  {
+    number: 103,
+    nameAr: "سورة العصر",
+    nameBn: "সূরা আল-'আসর",
+    type: "Makki",
+    ayahCount: 3,
+    file: "surah_103.json"
+  },
+  {
+    number: 104,
+    nameAr: "سورة الهمزة",
+    nameBn: "সূরা আল-হুমাযাহ",
+    type: "Makki",
+    ayahCount: 9,
+    file: "surah_104.json"
+  },
+
+  // Future: বাকিগুলো ধীরে ধীরে বাড়াবো
+];
